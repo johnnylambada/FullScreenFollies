23141f759801d479ca59a47dece49de497017e88
John Lombardo <john@lombardos.org>
2015-09-07 07:49:44 -0700

    Incorporated answers from stackoverflow http://stackoverflow.com/questions/32429103

--- app/src/main/java/com/sigseg/fullstreemfollies/FullScreenImageActivity.java
+++ app/src/main/java/com/sigseg/fullstreemfollies/FullScreenImageActivity.java
@@ -1,6 +1,7 @@
 
 package com.sigseg.fullstreemfollies;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,7 +18,23 @@ public class FullScreenImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        // karaokyo's approach
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+            // Set to full screen layout. The system notification bar overlays the top of the app
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        // Tan Tran's approach #1
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        // Tan Tran's approach #2
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
         Glide.with(this)
                 .load("http://i.huffpost.com/gen/2229248/images/o-PARALLEL-UNIVERSES-facebook.jpg")
                 .centerCrop()
